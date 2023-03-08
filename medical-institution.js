d28a281be5c850104f0146119496f2a5c7c1610a
borict <tamara.boric8@gmail.com>
2023-03-08 12:07:56 +0100

    Kreiran doktor

---
+++ medical-institution.js
@@ -0,0 +1,8 @@
+class Doktor {
+  constructor(ime, prezime, specijalizacija) {
+    this.ime = ime;
+    this.prezime = prezime;
+    this.specijalizacija = specijalizacija;
+    this.pacijenti = [];
+  }
+}
